7f93b514b961d9c4ef3c95af36781fe0e9b45841
tanu529 <tanu42475@gmail.com>
2026-01-29 20:58:02 +0530

    Add basic JavaScript variables, functions, and control flow

---
+++ script.js
@@ -0,0 +1,27 @@
+// Basic variables
+let name = "Tanu";
+let age = 20;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+// Function example
+function greet(user) {
+  return "Hello, " + user + "!";
+}
+
+console.log(greet(name));
+
+// Array and loop
+let skills = ["HTML", "CSS", "JavaScript"];
+
+for (let i = 0; i < skills.length; i++) {
+  console.log("Skill:", skills[i]);
+}
+
+// Conditional statement
+if (age >= 18) {
+  console.log("Eligible to learn advanced JavaScript");
+} else {
+  console.log("Keep learning basics");
+}
